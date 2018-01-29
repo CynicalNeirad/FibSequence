c4281b59d9d5da11ea8df0601be7c40e5f6ba3a1
Richard Spencer <rlspenc15@earlham.edu>
2018-01-29 10:25:17 -0500

    Changed the program to accept web inputs and to print them out there

--- src/main/java/com/example/fib/mainController.java
+++ src/main/java/com/example/fib/mainController.java
@@ -1,36 +1,30 @@
 package com.example.fib;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-
-import static java.lang.Character.getNumericValue;
-
 @RestController
 public class mainController {
 
-    @RequestMapping("/")
-    public String showIndex() {
-        Scanner numberInput = new Scanner(System.in);
-        int number = numberInput.nextInt();
+    @RequestMapping(value = "/goalnumber" , method = RequestMethod.GET)
+    public String showIndex(
+        @RequestParam("goalnumber") int number) {
         int i = 2;
         int sumNumbers = 0;
         ArrayList<Integer> fibSequence = new ArrayList();
         fibSequence.add(0);
         fibSequence.add(1);
 
-        while (fibSequence.get(i-1) <= number) {
+        while (fibSequence.get(i-1) < number) {
             fibSequence.add(fibSequence.get(i-2) + fibSequence.get(i-1));
             i++;
         }
         for (int n = 0; n < 4; n++) {
             sumNumbers = sumNumbers + fibSequence.get((i-1)-n);
         }
-
-        System.out.println(Arrays.toString(fibSequence.toArray()));
-        System.out.println(sumNumbers);
-    return null;
+        return (Arrays.toString(fibSequence.toArray()) + "<br/>" + sumNumbers);
     }
+
 }
