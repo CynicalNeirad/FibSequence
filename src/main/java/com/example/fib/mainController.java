0a15ab223b2637fc177833aba9e1f55bf486c634
Richard Spencer <rlspenc15@earlham.edu>
2018-01-29 10:49:59 -0500

    Small order change

--- src/main/java/com/example/fib/mainController.java
+++ src/main/java/com/example/fib/mainController.java
@@ -12,9 +12,9 @@ public class mainController {
     public String showIndex(
         @RequestParam("goalnumber") int goalNumber) {
         int i = 4;
-        ArrayList<Integer> fibSequence = new ArrayList<Integer>() {{add(0); add(1); add(1); add(2);}};  //Creates the array list and then .adds the two values to it in the same line
-        while (fibSequence.get(i-1) < goalNumber) {                                     //I need to do that to enable the following math, which is helped by the sequence always being the same
-            fibSequence.add(fibSequence.get(i - 3) + fibSequence.get(i - 4) + fibSequence.get(i - 2) + fibSequence.get(i - 1));               //And I'm using array lists as they don't have to have an assigned size
+        ArrayList<Integer> fibSequence = new ArrayList<Integer>() {{add(0); add(1); add(1); add(2);}};                                        //Creates the array list and then .adds the two values to it in the same line
+        while (fibSequence.get(i-1) < goalNumber) {                                                                                           //I need to do that to enable the following math, which is helped by the sequence always being the same
+            fibSequence.add(fibSequence.get(i - 1) + fibSequence.get(i - 2) + fibSequence.get(i - 3) + fibSequence.get(i - 4));               //And I'm using array lists as they don't have to have an assigned size
             i++;
         }
         return (Arrays.toString(fibSequence.toArray()));
