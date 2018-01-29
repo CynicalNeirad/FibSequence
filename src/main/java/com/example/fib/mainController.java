f8b8d9d212a55be30746acf10672e8b803566cd2
Richard Spencer <rlspenc15@earlham.edu>
2018-01-29 10:35:01 -0500

    Added comments for future clarity

--- src/main/java/com/example/fib/mainController.java
+++ src/main/java/com/example/fib/mainController.java
@@ -13,9 +13,9 @@ public class mainController {
         @RequestParam("goalnumber") int goalNumber) {
         int i = 2;
         int sumNumbers = 0;
-        ArrayList<Integer> fibSequence = new ArrayList<Integer>() {{add(0); add(1);}};
-        while (fibSequence.get(i-1) < goalNumber) {
-            fibSequence.add(fibSequence.get(i-2) + fibSequence.get(i-1));
+        ArrayList<Integer> fibSequence = new ArrayList<Integer>() {{add(0); add(1);}};  //Creates the array list and then .adds the two values to it in the same line
+        while (fibSequence.get(i-1) < goalNumber) {                                     //I need to do that to enable the following math, which is helped by the sequence always being the same
+            fibSequence.add(fibSequence.get(i-2) + fibSequence.get(i-1));               //And I'm using array lists as they don't have to have an assigned size
             i++;
         }
         for (int n = 0; n < 4; n++) {
