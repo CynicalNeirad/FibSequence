ee52b9681a1c6f8660703af1b39f43c5e93d1e20
Richard Spencer <rlspenc15@earlham.edu>
2018-01-29 10:46:56 -0500

    Added comments for future clarity

--- src/main/java/com/example/fib/mainController.java
+++ src/main/java/com/example/fib/mainController.java
@@ -11,17 +11,13 @@ public class mainController {
     @RequestMapping(value = "/goalnumber" , method = RequestMethod.GET)
     public String showIndex(
         @RequestParam("goalnumber") int goalNumber) {
-        int i = 2;
-        int sumNumbers = 0;
-        ArrayList<Integer> fibSequence = new ArrayList<Integer>() {{add(0); add(1);}};  //Creates the array list and then .adds the two values to it in the same line
+        int i = 4;
+        ArrayList<Integer> fibSequence = new ArrayList<Integer>() {{add(0); add(1); add(1); add(2);}};  //Creates the array list and then .adds the two values to it in the same line
         while (fibSequence.get(i-1) < goalNumber) {                                     //I need to do that to enable the following math, which is helped by the sequence always being the same
-            fibSequence.add(fibSequence.get(i-2) + fibSequence.get(i-1));               //And I'm using array lists as they don't have to have an assigned size
+            fibSequence.add(fibSequence.get(i - 3) + fibSequence.get(i - 4) + fibSequence.get(i - 2) + fibSequence.get(i - 1));               //And I'm using array lists as they don't have to have an assigned size
             i++;
         }
-        for (int n = 0; n < 4; n++) {
-            sumNumbers = sumNumbers + fibSequence.get((i-1)-n);
-        }
-        return (Arrays.toString(fibSequence.toArray()) + "<br/>" + sumNumbers);
+        return (Arrays.toString(fibSequence.toArray()));
     }
 
 }
